3d09de7fd25ef4450f2d9995e2dea713788f3006
Your Name <you@example.com>
2021-11-24 14:50:43 -0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('title', 'EDUAT')
+
+@section('content_header')
+    <h1>EDUAT</h1>
+@stop
+
+@section('content')
+    <div class ="card">
+        <div class ="card-header">
+            <h1 class = "card-tittle">Hola Mundo</h1>
+        </div>
+
+        <div class ="card-body">
+            <p>Prubeasdfghjklkjhgfsa</p>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
